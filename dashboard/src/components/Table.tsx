3e1030e504f1958ab126bdbd4c020e3479f4100d
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-05-19 17:07:25 -0400

    Add "Settings" tab to domains in dashboard

--- dashboard/src/components/Table.tsx
+++ dashboard/src/components/Table.tsx
@@ -8,7 +8,7 @@ import {
   TableRow,
 } from '@/components/ui/table'
 import { getApiKey } from '@/hooks/useLocalApiKey'
-import { useNames } from '@/hooks/useNamestone'
+import { useDomain, useNames } from '@/hooks/useNamestone'
 import { useParams } from 'react-router-dom'
 import { Button } from './ui/button'
 import { namestoneClient } from '@/lib/namestone'
@@ -16,14 +16,21 @@ import { toast } from 'sonner'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import { AlertCircle } from 'lucide-react'
 import { AddName } from './AddName'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { Input } from './ui/input'
+import { Label } from './ui/label'
+import { useEnsAvatar, useEnsName } from 'wagmi'
+import { Address } from 'viem'
+import { truncateAddress } from '@/lib/utils'
 
 export function NamesTable() {
   const { domain } = useParams()
+  const nsDomain = useDomain(domain)
   const apiKey = getApiKey(domain)
   const names = useNames(domain)
 
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-4">
       <div className="flex items-center justify-between gap-2">
         <h2 className="text-lg font-bold">{domain}</h2>
         {apiKey && (
@@ -41,51 +48,102 @@ export function NamesTable() {
         </Alert>
       )}
 
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-40">Name</TableHead>
-            <TableHead>Address</TableHead>
-            <TableHead>Actions</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {names.data?.map((name) => (
-            <TableRow key={name.name}>
-              <TableCell className="font-medium">{name.name}</TableCell>
-              <TableCell>{name.address}</TableCell>
-              <TableCell>
-                <Button
-                  size="sm"
-                  variant="destructive"
-                  onClick={async () => {
-                    const promise = namestoneClient(apiKey!).deleteName({
-                      domain: domain!,
-                      name: name.name,
-                    })
+      <Tabs defaultValue="names">
+        <TabsList className="w-full">
+          <TabsTrigger value="names">Names</TabsTrigger>
+          <TabsTrigger value="settings">Settings</TabsTrigger>
+        </TabsList>
 
-                    toast.promise(promise, {
-                      loading: 'Deleting...',
-                      success: 'Deleted!',
-                      error: 'Failed to delete',
-                    })
+        <TabsContent value="names">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-40">Name</TableHead>
+                <TableHead>Address</TableHead>
+                <TableHead>Actions</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {names.data?.map((name) => (
+                <TableRow key={name.name}>
+                  <TableCell className="font-medium">{name.name}</TableCell>
+                  <TableCell>{name.address}</TableCell>
+                  <TableCell>
+                    <Button
+                      size="sm"
+                      variant="destructive"
+                      onClick={async () => {
+                        const promise = namestoneClient(apiKey!).deleteName({
+                          domain: domain!,
+                          name: name.name,
+                        })
 
-                    names.refetch()
-                  }}
-                >
-                  Delete
-                </Button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TableCell colSpan={3}>Total</TableCell>
-            <TableCell className="text-right">{names.data?.length}</TableCell>
-          </TableRow>
-        </TableFooter>
-      </Table>
+                        toast.promise(promise, {
+                          loading: 'Deleting...',
+                          success: 'Deleted!',
+                          error: 'Failed to delete',
+                        })
+
+                        names.refetch()
+                      }}
+                    >
+                      Delete
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TableCell colSpan={3}>Total</TableCell>
+                <TableCell className="text-right">
+                  {names.data?.length}
+                </TableCell>
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </TabsContent>
+
+        <TabsContent value="settings" className="flex flex-col gap-4 pt-3">
+          <div className="flex flex-col gap-1.5">
+            <Label className="text-base font-semibold">API Key</Label>
+            <Input disabled value={apiKey ?? ''} />
+          </div>
+
+          <div className="flex flex-col gap-1.5">
+            <Label className="text-base font-semibold">Admins</Label>
+            <div className="flex flex-col gap-2">
+              {nsDomain.data?.admins.map((admin) => (
+                <InlineProfile key={admin} address={admin} />
+              ))}
+            </div>
+          </div>
+        </TabsContent>
+      </Tabs>
+    </div>
+  )
+}
+
+function InlineProfile({ address }: { address: Address }) {
+  const { data: ensName } = useEnsName({ address })
+  const { data: ensAvatar } = useEnsAvatar({
+    name: ensName ?? undefined,
+  })
+
+  return (
+    <div className="flex items-center gap-2">
+      <div className="flex items-center gap-1.5">
+        <div className="h-6 w-6 overflow-hidden rounded bg-gray-200">
+          {ensAvatar && (
+            <img
+              src={ensAvatar}
+              alt={ensName!}
+              className="h-full w-full object-cover"
+            />
+          )}
+        </div>
+        <span>{ensName || truncateAddress(address)}</span>
+      </div>
     </div>
   )
 }
--- dashboard/src/hooks/useNamestone.ts
+++ dashboard/src/hooks/useNamestone.ts
@@ -1,6 +1,8 @@
 import { useQuery } from '@tanstack/react-query'
 import { getApiKey } from './useLocalApiKey'
 import { namestoneClient } from '@/lib/namestone'
+import { DomainData } from '@namestone/namestone-sdk'
+import type { Address } from 'viem'
 
 export function useNames(domain: string | undefined) {
   return useQuery({
@@ -11,6 +13,21 @@ export function useNames(domain: string | undefined) {
 
       const client = namestoneClient(apiKey)
       return client.getNames({ domain })
+    },
+  })
+}
+
+export function useDomain(domain: string | undefined) {
+  return useQuery({
+    queryKey: ['domain', domain],
+    queryFn: async () => {
+      const apiKey = getApiKey(domain)
+      if (!domain || !apiKey) return null
+
+      const client = namestoneClient(apiKey)
+      const res = await client.getDomain({ domain })
+      // Our API adds the admins, which is different from the official Namestone API
+      return res as DomainData[] & { admins: Address[] }
     },
   })
 }
