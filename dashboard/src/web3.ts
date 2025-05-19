3c8539475872bbf3f5b0e57e9e68f35459dd309b
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-05-19 18:56:04 -0400

    Add RPC_URL env var

--- dashboard/src/web3.ts
+++ dashboard/src/web3.ts
@@ -1,9 +1,11 @@
-import { http, createConfig } from 'wagmi'
+import { createConfig, http } from 'wagmi'
 import { mainnet } from 'wagmi/chains'
+
+import { env } from './lib/env'
 
 export const wagmiConfig = createConfig({
   chains: [mainnet],
   transports: {
-    [mainnet.id]: http(),
+    [mainnet.id]: http(env.rpcUrl),
   },
 })
