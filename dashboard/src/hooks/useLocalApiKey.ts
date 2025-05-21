b82ebd5ff85de8a62aba23f7b17eb93171e6ff6d
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-05-21 00:49:58 -0400

    Remove unused package

--- dashboard/src/hooks/useLocalApiKey.ts
+++ dashboard/src/hooks/useLocalApiKey.ts
@@ -1,14 +1,3 @@
-import { useLocalStorage } from 'usehooks-ts'
-
-export function useLocalApiKey(domain: string | undefined) {
-  const [apiKey, setApiKey, deleteApiKey] = useLocalStorage<string | undefined>(
-    `namestone_${domain}`,
-    ''
-  )
-
-  return { apiKey, setApiKey, deleteApiKey }
-}
-
 export function setApiKey(domain: string, apiKey: string) {
   localStorage.setItem(`namestone_${domain}`, apiKey)
 }
